a093e4a47fc4e2fffebc8827df7826cd6bf4ad47
jianping.xwh <jianping.xwh@taobao.com>
2016-01-16 18:16:12 +0800

    canvas测试

---
+++ test.js
@@ -0,0 +1,19 @@
+var Canvas = require('canvas');
+
+var canvas = new Canvas(width, height);
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, width, height);
+
+ctx.fillStyle = "#000";
+ctx.font = '48px';
+ctx.fillText("测试", 10, 50);
+
+// Create streams
+var out = fs.createWriteStream('../lris-koa-demo/public/test.jpg');
+var stream = canvas.jpegStream();
+
+stream.on('data', function(chunk){
+    console.log(chunk);
+    out.write(chunk);
+});
